60dd186313941b4cecdf86edd6ee497d32528359
danush4756-ai <danush4756@gmail.com>
2026-02-19 19:01:33 +0530

    ReverseArrayList

--- src/List/ReverseArrayList.java
+++ src/List/ReverseArrayList.java
@@ -3,7 +3,7 @@ package List;
 import java.util.*;
 public class ReverseArrayList {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList (Arrays.asList(1, 2, 3, 4, 5));
+        List<Integer> list = new ArrayList <>(Arrays.asList(1, 2, 3, 4, 5));
         Collections.reverse(list);
         System.out.println("Reversed ArrayList: " + list);
     }
